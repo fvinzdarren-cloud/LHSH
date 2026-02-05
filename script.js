946772dbaade3757626ad813295bc1d8ae60b49b
fvinzdarren-cloud <fvinzdarren@gmail.com>
2026-02-06 00:44:25 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
 
-noBtn.addEventListener("mouseover", moveButton);
-noBtn.addEventListener("click", moveButton);
+let scale = 1;
 
-function moveButton() {
+
+
+function moveAndGrow() { 
   const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
   const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
 
+  const x = Math.random() * maxX;
+  const y = Math.random() * maxY;
+
   noBtn.style.left = `${x}px`;
   noBtn.style.top = `${y}px`;
+
+  scale += 0.2;
+  yesBtn.style.transform = `scale(${scale})`;
 }
+noBtn.addEventListener("mouseover", moveButton);
+noBtn.addEventListener("click", moveButton);
